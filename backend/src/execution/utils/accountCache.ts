32481e3bcc236b4a0162d9163f9e45e5ae7cb6a4
thelawenforcer <lawrence.mccurrach@gmail.com>
2025-11-06 23:42:09 +0100

    fix

--- backend/src/execution/utils/accountCache.ts
+++ backend/src/execution/utils/accountCache.ts
@@ -85,9 +85,12 @@ export class AccountDataCache {
       if (cachedEntry && now < cachedEntry.expiresAt) {
         cached[i] = cachedEntry.data;
       } else {
-        const pubkey: PublicKey = typeof addresses[i] === 'string' 
-          ? new PublicKey(addresses[i]) 
-          : addresses[i];
+        let pubkey: PublicKey;
+        if (typeof addresses[i] === 'string') {
+          pubkey = new PublicKey(addresses[i]);
+        } else {
+          pubkey = addresses[i];
+        }
         toFetch.push({ index: i, pubkey });
       }
     }
